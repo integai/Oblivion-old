2fdd2c9932c10f0dfe3a4f0f384a8dc153102894
oblvisheee <molniya213y@gmail.com>
2024-01-08 01:52:35 +0300

    fixes: disabled verification by 1 condition for a while

--- utils/isBlockValid.cpp
+++ utils/isBlockValid.cpp
@@ -9,12 +9,17 @@ bool isBlockValid(const Block& block, const Blockchain& blockchain) {
         std::cerr << "Block hash is not valid.\n";
         return false;
     }
-
-    if (latestBlock.GetHash() != block.GetPrevHash() && block.GetPrevHash() != "0") {
+    //TODO: Fix validation for Genesis Block (Currently disabled)
+    /*if (block.GetHash() == blockchain.getGenesisBlock().GetHash()) {
+        if (blockchain.IsEmpty()) {
+            std::cerr << "Blockchain is empty. Cannot validate block.\n";
+            return false;
+        }
+        return true;
+    } else if (latestBlock.GetHash() != block.GetPrevHash()) {
         std::cerr << "Block does not continue from the latest block in the blockchain.\n";
         return false;
-    }
+    }*/
 
     return true;
 }
-
