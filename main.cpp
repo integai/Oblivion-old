964003f5d3dfb5331c1633f628581c81c9d8f8b9
oblvisheee <molniya213y@gmail.com>
2024-01-08 13:51:35 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,22 @@
 #include "launch.h"
+#include <iostream>
 using namespace std;
 
 int main() {
-    test_blockchain_creation();
-    /* test_transaction(); */
+    int choice;
+    cout << "Select the test to run (1 - Blockchain, 2 - Transaction): ";
+    cin >> choice;
+    switch(choice) {
+        case 1:
+            test_blockchain_creation();
+            break;
+        case 2:
+            test_transaction();
+            break;
+        default:
+            cout << "Invalid choice. Please select either 1 or 2." << endl;
+            break;
+    }
+    return 0;
 }
 
